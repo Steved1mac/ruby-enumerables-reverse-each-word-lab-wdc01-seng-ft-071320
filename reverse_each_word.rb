f7d66d1b21c6b9eadf6d835da0190cf06311bb9b
Steven McAllister <steved1mac@gmail.com>
2020-07-08 16:46:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
   sentence = ("Hello there, and how are you?")
 def reverse_each_word(sentence)
-  new_array = []
-  (sentence).split = new_array
+  new_array = sentence.split 
   new_array.each do |string|
 end 
 end
