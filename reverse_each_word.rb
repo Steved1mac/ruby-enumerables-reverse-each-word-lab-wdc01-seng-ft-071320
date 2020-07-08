f111dc44902eff2a70d9ec2c30eaf7ae0b279a70
Steven McAllister <steved1mac@gmail.com>
2020-07-08 16:29:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word("Hello there, and how are you?")
   new_array = []
-  "Hello there, and how are you?" = sentence
+
   (sentence).split = new_array
 def reverse_each_word(sentence)
 end 
